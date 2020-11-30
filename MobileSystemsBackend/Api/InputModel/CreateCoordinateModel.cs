6bfc99c571ef905a36c6aa7fa8e3679f218ade29
Daniel Skjold Toft <dstoft@hotmail.com>
2020-11-30 15:54:34 +0100

    Implemented Coordinate API Implemented: * Controller class * CreateCoordinateModel * ReadCoordinateModel

---
+++ MobileSystemsBackend/Api/InputModel/CreateCoordinateModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MobileSystemsBackend.Api.InputModel
+{
+    public class CreateCoordinateModel
+    {
+        // ReSharper disable once InconsistentNaming
+        public long time { get; set; }
+        // ReSharper disable once InconsistentNaming
+        public double latitude { get; set; }
+        // ReSharper disable once InconsistentNaming
+        public double longitude { get; set; }
+    }
+}
---
+++ MobileSystemsBackend/Api/OutputModel/ReadCoordinateModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MobileSystemsBackend.Api.OutputModel
+{
+    public class ReadCoordinateModel
+    {
+        public Guid Id { get; set; }
+        public long EpochTime { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
